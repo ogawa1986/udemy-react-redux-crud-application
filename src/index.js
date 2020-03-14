ce0d0fc7b7b803882df36a8dee283d147d62b119
竹中誠矢 <seiya@takenakamakotoyanoiMac.local>
2020-03-15 01:20:13 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const store = createStore(reducer)
 ReactDOM.render(
     <Provider store={store}>
         <App />
-    </Provider>
+    </Provider>,
     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
