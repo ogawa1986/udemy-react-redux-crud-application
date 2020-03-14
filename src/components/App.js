b33452c303e6fdbbf75f279d5c835a360fe4aac0
竹中誠矢 <seiya@takenakamakotoyanoiMac.local>
2020-03-15 01:37:49 +0900

    React Redux基本編終了

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { increment, decrement } from '../actions';
 
-function App() {
-  return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => {console.log("I am clicked!")}} />
-    </React.Fragment>
-  );
+class App extends Component {
+  render() {
+    const props = this.props;
+    return (
+      <React.Fragment>
+        <div>value: { props.value }</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
+      </React.Fragment>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => ({ value: state.count.value });
+// const mapDispatchToProps = dispatch => ({
+//   increment: () => dispatch(increment()),
+//   decrement: () => dispatch(dispatch())
+// })
+
+const mapDispatchToProps = ({ increment, decrement })
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
